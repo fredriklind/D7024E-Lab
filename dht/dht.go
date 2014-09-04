83e6dc31aabbf2b23e42069cb3f9b7655f99c2c7
Fredrik Lind <fredrik@editerat.se>
2014-09-04 13:13:28 +0200

    Added struct for DHTNode and initializer

--- dht/dht.go
+++ dht/dht.go
@@ -1,2 +1,14 @@
 package dht
 
+type DHTNode struct {
+	id, adress, port *string
+}
+
+func makeDHTNode(id *string, adress *string, port *string) *DHTNode {
+
+	if id == nil {
+		id = generateNodeId()
+	}
+
+	return &DHTNode{id: id, adress: adress, port: port}
+}
