d3c3c52e13bce65a5d891784a16b440861a321df
Fredrik Lind <fredrik@editerat.se>
2014-09-17 19:29:25 +0200

    Now testing calcFinger for 07, does not seem to work

--- dht/dht_test.go
+++ dht/dht_test.go
@@ -143,68 +143,32 @@ func TestFinger3bits(t *testing.T) {
 	id0 := "00"
 	id1 := "01"
 	id2 := "02"
-	//	id3 := "03"
-	//id4 := "04"
-	/*id5 := "05"
+	id3 := "03"
+	id4 := "04"
+	id5 := "05"
 	id6 := "06"
-	id7 := "07"*/
+	id7 := "07"
 
 	node0 := makeDHTNode(&id0, "localhost", "1111")
 	node1 := makeDHTNode(&id1, "localhost", "1112")
 	node2 := makeDHTNode(&id2, "localhost", "1113")
-	//	node3 := makeDHTNode(&id3, "localhost", "1114")
-	//	node4 := makeDHTNode(&id4, "localhost", "1115")
-	/*node5 := makeDHTNode(&id5, "localhost", "1116")
+	node3 := makeDHTNode(&id3, "localhost", "1114")
+	node4 := makeDHTNode(&id4, "localhost", "1115")
+	node5 := makeDHTNode(&id5, "localhost", "1116")
 	node6 := makeDHTNode(&id6, "localhost", "1117")
-	node7 := makeDHTNode(&id7, "localhost", "1118")*/
+	node7 := makeDHTNode(&id7, "localhost", "1118")
 
-	//	initTwoNodeRing(node1, node2)
-	//	node1.printRing()
 	node2.join(nil)
-	//	node2.printFingers()
-
-	node1.join(node2)
-	//	node2.printFingers()
-	//	node1.printFingers()
-
 	node0.join(node2)
-	//	node2.printFingers()
-	//	node1.printFingers()
-	//	node0.printFingers()
-
-	//	node3.join(node1)
-	//	node0.printRing()
-	//	node4.join(node1)
-	//node1.printRing()
-	/*node5.join(node1)
+	node1.join(node2)
+	node3.join(node1)
+	node4.join(node1)
+	node5.join(node1)
 	node6.join(node1)
 	node7.join(node2)
 
-
-
-
-	node3.printFingers()
-	node4.printFingers()
-	node5.printFingers()
-	node6.printFingers()
-	node7.printFingers()*/
-
-	//	var d big.Int
-	//	d = distance(node0.id, node1.id, 3)
-
-	fmt.Println("------------------------------------------------------------------------------------------------")
-	fmt.Println("RING STRUCTURE")
-	fmt.Println("------------------------------------------------------------------------------------------------")
-	//	node2.printRing()
-	fmt.Println("------------------------------------------------------------------------------------------------")
-
-	/*
-		node3.testCalcFingers(1, 3)
-		fmt.Println("")
-		node3.testCalcFingers(2, 3)
-		fmt.Println("")
-		node3.testCalcFingers(3, 3)
-	*/
+	var calc, _ = calcFinger([]byte("07"), 1, 3)
+	fmt.Printf("calcFinger: '%s'\n", calc)
 }
 
 func TestDebug(t *testing.T) {
