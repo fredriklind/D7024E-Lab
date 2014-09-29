bd729b7b3aaac16e5698f1ff1902970936cd3f64
Sofie Nilsson <sofnii-9@student.ltu.se>
2014-09-29 12:57:18 +0200

    Ready for release 1

--- dht/dht2.go
+++ dht/dht2.go
@@ -2,7 +2,6 @@ package dht
 
 import (
 	"fmt"
-	"encoding/hex"
 )
 
 func (n *DHTNode) printNode2() {
@@ -40,60 +39,74 @@ func (n *DHTNode) printRing2() {
 //	fmt.Println()
 }
 
-func hexStringToByteArr(hexId string) []byte {
-	var hexbytes []byte
-	hexbytes, _ = hex.DecodeString(hexId)
-	return hexbytes
-}
-
 // Returns the node who is responsible for the data corresponding to hashKey, traversing the ring linearly
 func (n *DHTNode) linearLookup(hashKey string) *DHTNode {
 	if between(hexStringToByteArr(nextId(n.predecessor.id)), hexStringToByteArr(nextId(n.id)), hexStringToByteArr(hashKey)) {
 		return n
 	} else {
-		return n.predecessor.lookup(hashKey)
+		return n.predecessor.linearLookup(hashKey)
 	}
 }
 
-// Returns the node who is responsible for the data corresponding to id, traversing the ring using finger tables
-func (n *DHTNode) lookup2(id string) *DHTNode {
-//	fmt.Printf("Performing lookup from node %s\n", n.id)
-	// n responsible for id
-	if between(hexStringToByteArr(nextId(n.predecessor.id)), hexStringToByteArr(nextId(n.id)), hexStringToByteArr(id)) {
-//		fmt.Printf("%s E (%s, %s], eg. [%s, %s) \n", id, n.predecessor.id, n.id, nextId(n.predecessor.id), nextId(n.id))
-		return n
-	// otherwise use fingers of n, starting with the one that is furthest away, to find responsible node
-	} else {
-		for i:=m; i>=1; i-- {
 
-//			fmt.Printf("i=%d\n", i)
-			// special case - when n´s finger points to itself
-			if (n.fingerTable[i].node.id == n.id) {
+func (n *DHTNode) printRing() {
+	n.printNode()
+	var visited []string
+	visited = append(visited, n.id)
+	newn := n.successor()
 
-				// what to do?
-				// go to next finger...
-//				fmt.Println("Finger points to node itself<-------------------")
+	for !stringInSlice(newn.id, visited) {
+		newn.printNode()
+		visited = append(visited, newn.id)
+		newn = newn.successor()
+	}
+}
 
+func (n *DHTNode) printNode() {
+	fmt.Println("------------------------")
+	fmt.Printf("Node:        %s\n", n.id)
+	fmt.Printf("Predecessor: %s\n", n.predecessor.id)
+	n.printFingers()
+	//fmt.Println("------------------------\n")
+}
 
-			} else if between(hexStringToByteArr(n.fingerTable[i].node.id), hexStringToByteArr(n.id), hexStringToByteArr(id)) {
-//				fmt.Printf("%s E [%s,%s)\n", id, n.fingerTable[i].node.id, n.id)
-//				fmt.Printf("Go to node %s and perform lookup on %s\n", n.fingerTable[i].node.id, id)
-				return ((n.fingerTable[i].node).lookup2(id))
-			}
+func (n *DHTNode) printFingers() {
+	fmt.Println("| startId  |   node.id |")
+	for i := 1; i <= m; i++ {
+		fmt.Printf("| %s       |        %s |\n", n.fingerTable[i].startId, n.fingerTable[i].node.id)
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
 		}
-		/*// if id is not between any finger and n - then id must be between n and its successor
-		if !(n.fingerTable[1].node.id == n.id) {
-			return n.fingerTable[1].node
-		}*/
-		return n.fingerTable[1].node
 	}
-}	
-
-// lookup of finger.node for the case when a second node is added to a ring with only one node
-func (n *DHTNode) specLookup(newNode *DHTNode, startId string) * DHTNode {
-	if between(hexStringToByteArr(nextId(newNode.id)), hexStringToByteArr(nextId(n.id)), hexStringToByteArr(startId)) {
-		return n
-	}
-	return newNode
+	return false
 }
 
+// could be used in lookup
+func (n *DHTNode) findSuccessor(id string) *DHTNode {
+	predecessor := n.findPredecessor(id)
+	return predecessor.successor()
+}
+
+func (n *DHTNode) findPredecessor(id string) *DHTNode {
+	n2 := n
+	for !between(hexStringToByteArr(nextId(n2.id)), hexStringToByteArr(nextId(n2.successor().id)), hexStringToByteArr(id)) {
+		n2 = n2.closestPrecedingFinger(id)
+	}
+	return n2
+}
+
+func (n *DHTNode) closestPrecedingFinger(id string) *DHTNode {
+	for i := m; i > 0; i-- {
+		if between(hexStringToByteArr(nextId(n.id)), hexStringToByteArr(id), hexStringToByteArr(n.fingerTable[i].node.id)) {
+			//			fmt.Printf(" %s\n", n.fingerTable[i].node.id)
+			return n.fingerTable[i].node
+		}
+	}
+	//	fmt.Printf(" %s\n", n.id)
+	return n
+}
