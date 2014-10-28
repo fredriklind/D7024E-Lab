85723948f7220a9219d36e19dd53fdc5872da4b1
Sofie Nilsson <sofnii-9@student.ltu.se>
2014-10-28 10:20:51 +0100

    New files for obj5

---
+++ manage_dht/manage_dht.go
@@ -0,0 +1,8 @@
+package manage_dht
+
+func main() {
+	// start webserver
+}
+
+// this file should contain the logic for the API-requests:
+// startNewNode, nodeLeavesRing?, updateData (should return all nodes current stored data)?
---
+++ manage_dht/server_.go
@@ -0,0 +1,4 @@
+package manage_dht
+
+// this file should contain code for starting up a webserver,
+// the REST-API with its routes to functions in manage_dht.go
